c9ce947c09b2e3ebeb4d1fa9af3f6eceaca21e20
timbehh1 <timib895@gmail.com>
2026-04-15 09:47:33 -0400

    Update Monte Carlo.R
    Monte Carlo with the wrangling template normally used for class.

--- Monte Carlo.R
+++ Monte Carlo.R
@@ -1,14 +1,24 @@
-##Monte Carlo Work
+# Short Title ----
+## Monte Carlo integration plot
+## Estimate area under chi-square pdf
 
+# Step 1: Load Packages ----
+## Needed Packages:
 library(ggplot2)
 library(patchwork)
 
+# Step 2: Load Data ----
+## Needed data:
 set.seed(1)
 
+# Step 3: short description ----
+## define function
 f <- function(x){
   dchisq(x, df = 3)
 }
 
+# Step 4: short description ----
+## monte carlo plotting
 mc_plot <- function(n){
   
   x <- runif(n, 0, 10)
@@ -32,10 +42,19 @@ mc_plot <- function(n){
     theme(legend.position = "none")
 }
 
+## Step 4A: short description ----
 p1 <- mc_plot(10)
 p2 <- mc_plot(100)
+
+## Step 4B: short description ----
 p3 <- mc_plot(1000)
 p4 <- mc_plot(10000)
 
+## Repeat making steps as needed
 (p1 + p2) / (p3 + p4)
 
+
+
+MC
+
+##Monte Carlo Work
